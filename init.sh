a0413d6d36f256f9b622fbe96ae6ca7be27d7364
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2022-10-19 13:08:02 +0200

    chore: update configurations to use the default Docker image

--- init.sh
+++ init.sh
@@ -17,18 +17,17 @@ echo "Done."
 
 echo "-------------------"
 echo "Check context directory..."
-mkdir -p ${CONTEXT_DIR}
+if [ ! -d "${CONTEXT_DIR}" ]; then
+  mkdir -p ${CONTEXT_DIR}
+fi
 echo "Done."
 
 
 echo "-------------------"
 echo "PGAdmin: prepare data folder and change the owner..."
-#if [ ! -f "${CONTEXT_DIR}/data-pgadmin/.initialized" ]; then
- # mkdir -p ${CONTEXT_DIR}/data-pgadmin/sessions
- # mkdir -p ${CONTEXT_DIR}/data-pgadmin/storage
-#  mkdir -p ${CONTEXT_DIR}/data-pgadmin/azurecredentialcache
-#  touch "${CONTEXT_DIR}/data-pgadmin/.initialized"
-#  sudo chown -R 5050:5050 ${CONTEXT_DIR}/data-pgadmin
-#fi
+if [ ! -d "${CONTEXT_DIR}/data-pgadmin" ]; then
+  mkdir -p "${CONTEXT_DIR}/data-pgadmin"
+  sudo chown -R 5050:5050 "${CONTEXT_DIR}/data-pgadmin"
+fi
 echo "Done."
 
